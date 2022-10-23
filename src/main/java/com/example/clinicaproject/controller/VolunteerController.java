d38078f7a1f760b43d381efb65424ac682b47b3c
Natalia Pirogova <nataliapirogova1995@gmail.com>
2022-10-23 19:29:47 +0300

    attribute done for volunteerHabbits filter

--- src/main/java/com/example/clinicaproject/controller/VolunteerController.java
+++ src/main/java/com/example/clinicaproject/controller/VolunteerController.java
@@ -3,8 +3,7 @@ package com.example.clinicaproject.controller;
 import com.example.clinicaproject.model.Volunteer;
 import com.example.clinicaproject.model.VolunteerHabitsInfo;
 import com.example.clinicaproject.model.VolunteerPrimaryHealthInfo;
-import com.example.clinicaproject.model.enums.Gender;
-import com.example.clinicaproject.model.enums.Smoking;
+import com.example.clinicaproject.model.enums.*;
 import com.example.clinicaproject.service.VolunteerHabitsInfoService;
 import com.example.clinicaproject.service.VolunteerPrimaryHealthInfoService;
 import com.example.clinicaproject.service.VolunteerService;
@@ -102,7 +101,7 @@ public class VolunteerController {
 
     @PostMapping("/volunteersAll")
     public ModelAndView volunteersAll1(@RequestParam(value = "DoB", required = false) String DoB,
-            @RequestParam(value = "email", required = false) String email,
+                                       @RequestParam(value = "email", required = false) String email,
                                        @RequestParam(value = "firstName", required = false) String firstName,
                                        @RequestParam(value = "middleName", required = false) String middleName,
                                        @RequestParam(value = "lastName", required = false) String lastName,
@@ -130,6 +129,7 @@ public class VolunteerController {
                                        @RequestParam(value = "gender", required = false) String gender,
                                        ModelAndView modelAndView) {
         List<Volunteer> volunteers = volunteerService.allVolunteers();
+        modelAndView.addObject("volunteers", volunteers);
         if (DoB.length() != 0) {
             volunteers = volunteers.stream()
                     .filter(v -> v.getDoB().equals(LocalDate.parse(DoB))).collect(Collectors.toList());
@@ -176,40 +176,136 @@ public class VolunteerController {
 
     @GetMapping("/volunteersAllByHabitsInfo")
     public ModelAndView volunteersAllByHabitsInfo(ModelAndView modelAndView) {
-        List<VolunteerHabitsInfo> volunteerHabitsInfoList = volunteerHabitsInfoService.allVolunteerHabitsInfo();
-        modelAndView.addObject("volunteerHabitsInfos", volunteerHabitsInfoList);
+        List<VolunteerHabitsInfo> volunteersAllByHabitsInfo = volunteerHabitsInfoService.allVolunteerHabitsInfo();
+        modelAndView.addObject("volunteersAllByHabitsInfo", volunteersAllByHabitsInfo);
         modelAndView.setViewName("volunteersAllByHabitsInfo");
         return modelAndView;
     }
 
     @PostMapping("/volunteersAllByHabitsInfo")
-    public ModelAndView volunteersAllByHabitsInfoFiltered(ModelAndView modelAndView,
-                                                          RedirectAttributes ra,
-                                                          @ModelAttribute("newVolunteerHabitsInfo") VolunteerHabitsInfo newVolunteerHabitsInfo) {
-        List<VolunteerHabitsInfo> volunteerHabitsInfoList = volunteerHabitsInfoService.findAllBySmoking(Smoking.NO);
-//        ra.addFlashAttribute();
-//        VolunteerHabitsInfo volunteerHabitsInfoFilter = (VolunteerHabitsInfo) httpSession.getAttribute("newVolunteerHabitsInfo");
-//        volunteerHabitsInfoFilter.setId(-1);
-//        volunteerHabitsInfoService.addVolunteerHabitsInfo(volunteerHabitsInfoFilter);
-        modelAndView.setViewName("redirect:/volunteersAllByHabitsInfo");
+    public ModelAndView volunteersAllByHabitsInfoFiltered(
+            @RequestParam(value = "smoking", required = false) String smoking,
+            @RequestParam(value = "takingDrugs", required = false) String takingDrugs,
+            @RequestParam(value = "takingMedicines", required = false) String takingMedicines,
+            @RequestParam(value = "pregnantNow", required = false) String pregnantNow,
+            @RequestParam(value = "isPlanningPregnancy", required = false) String isPlanningPregnancy,
+            @RequestParam(value = "vegetarian", required = false) String vegetarian,
+            @RequestParam(value = "takingHormonalContraceptives", required = false) String takingHormonalContraceptives,
+            @RequestParam(value = "sport", required = false) String sport,
+            @RequestParam(value = "alcohol", required = false) String alcohol,
+            ModelAndView modelAndView,
+            RedirectAttributes ra) {
+        ra.addAttribute("smoking", smoking)
+                .addAttribute("takingDrugs", takingDrugs)
+                .addAttribute("takingMedicines", takingMedicines)
+                .addAttribute("pregnantNow", pregnantNow)
+                .addAttribute("isPlanningPregnancy", isPlanningPregnancy)
+                .addAttribute("vegetarian", vegetarian)
+                .addAttribute("takingHormonalContraceptives", takingHormonalContraceptives)
+                .addAttribute("sport", sport)
+                .addAttribute("alcohol", alcohol);
+        modelAndView.setViewName("redirect:/volunteersAllByHabitsInfoFiltered");
         return modelAndView;
     }
-//
-//    @GetMapping("/volunteersAllByHabitsInfoFiltered")
-//    public ModelAndView volunteersAllByHabitsInfoFiltered(ModelAndView modelAndView) {
-//        VolunteerHabitsInfo volunteerHabitsInfoFilter = volunteerHabitsInfoService.findVolunteerHabitsInfo(-1);
-//
-//        modelAndView.addObject("volunteerHabitsInfoFiltered", volunteerHabitsInfoFiltered);
-//        modelAndView.setViewName("volunteersAllByHabitsInfo");
-//        return modelAndView;
-//    }
 
+    @GetMapping("/volunteersAllByHabitsInfoFiltered")
+    public ModelAndView volunteersAllByHabitsInfo2(@RequestParam(value = "smoking", required = false) String smoking,
+                                                   @RequestParam(value = "takingDrugs", required = false) String takingDrugs,
+                                                   @RequestParam(value = "takingMedicines", required = false) String takingMedicines,
+                                                   @RequestParam(value = "pregnantNow", required = false) String pregnantNow,
+                                                   @RequestParam(value = "isPlanningPregnancy", required = false) String isPlanningPregnancy,
+                                                   @RequestParam(value = "vegetarian", required = false) String vegetarian,
+                                                   @RequestParam(value = "takingHormonalContraceptives", required = false) String takingHormonalContraceptives,
+                                                   @RequestParam(value = "sport", required = false) String sport,
+                                                   @RequestParam(value = "alcohol", required = false) String alcohol,
+                                                   ModelAndView modelAndView) {
+        List<VolunteerHabitsInfo> volunteersAllByHabitsInfo = volunteerHabitsInfoService.allVolunteerHabitsInfo();
+        modelAndView.addObject("volunteersAllByHabitsInfo", volunteersAllByHabitsInfo);
+        switch (Smoking.valueOf(smoking)) {
+            case NO -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getSmoking().equals(Smoking.NO)).collect(Collectors.toList());
+            case SMOKE_EARLIER -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getSmoking().equals(Smoking.SMOKE_EARLIER)).collect(Collectors.toList());
+            case SMOKE_SEVERAL_TIMES_A_MONTH -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getSmoking().equals(Smoking.SMOKE_SEVERAL_TIMES_A_MONTH)).collect(Collectors.toList());
+            case SMOKE_1_TIME_A_WEEK_OR_MORE_OFTEN -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getSmoking().equals(Smoking.SMOKE_1_TIME_A_WEEK_OR_MORE_OFTEN)).collect(Collectors.toList());
+        }
+        switch (TakingDrugs.valueOf(takingDrugs)) {
+            case NO -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getTakingDrugs().equals(TakingDrugs.NO)).collect(Collectors.toList());
+            case NOT_NOW_BUT_EARLIER -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getTakingDrugs().equals(TakingDrugs.NOT_NOW_BUT_EARLIER)).collect(Collectors.toList());
+            case YES -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getTakingDrugs().equals(TakingDrugs.YES)).collect(Collectors.toList());
+        }
+        if (takingMedicines.equals("true")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(VolunteerHabitsInfo::isTakingMedicines).collect(Collectors.toList());
+        }
+        if (takingMedicines.equals("false")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> (!v.isTakingMedicines())).collect(Collectors.toList());
+        }
+        if (pregnantNow.equals("true")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(VolunteerHabitsInfo::isPregnantNow).collect(Collectors.toList());
+        }
+        if (pregnantNow.equals("false")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> (!v.isPregnantNow())).collect(Collectors.toList());
+        }
+        switch (PlanningPregnancy.valueOf(isPlanningPregnancy)) {
+            case PLANNING_THIS_YEAR -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getIsPlanningPregnancy().equals(PlanningPregnancy.PLANNING_THIS_YEAR)).collect(Collectors.toList());
+            case PLANNING_A_YEAR_AFTER_OR_LATER -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getIsPlanningPregnancy().equals(PlanningPregnancy.PLANNING_A_YEAR_AFTER_OR_LATER)).collect(Collectors.toList());
+            case NOT_PLANNING -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getIsPlanningPregnancy().equals(PlanningPregnancy.NOT_PLANNING)).collect(Collectors.toList());
+        }
+        if (vegetarian.equals("true")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(VolunteerHabitsInfo::isVegetarian).collect(Collectors.toList());
+        }
+        if (vegetarian.equals("false")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> (!v.isVegetarian())).collect(Collectors.toList());
+        }
+        if (takingHormonalContraceptives.equals("true")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(VolunteerHabitsInfo::isTakingHormonalContraceptives).collect(Collectors.toList());
+        }
+        if (takingHormonalContraceptives.equals("false")) {
+            volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> (!v.isTakingHormonalContraceptives())).collect(Collectors.toList());
+        }
+        switch (Sport.valueOf(sport)) {
+            case NO -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getSport().equals(Sport.NO)).collect(Collectors.toList());
+            case PROFESSIONAL -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getSport().equals(Sport.PROFESSIONAL)).collect(Collectors.toList());
+            case AMATEUR_SPORTS -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getSport().equals(Sport.AMATEUR_SPORTS)).collect(Collectors.toList());
+        }
+        switch (Alcohol.valueOf(alcohol)) {
+            case NO -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getAlcohol().equals(Alcohol.NO)).collect(Collectors.toList());
+            case EVERY_DAY -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getAlcohol().equals(Alcohol.EVERY_DAY)).collect(Collectors.toList());
+            case ONLY_ON_HOLIDAYS -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getAlcohol().equals(Alcohol.ONLY_ON_HOLIDAYS)).collect(Collectors.toList());
+            case ONE_OR_TWO_TIMES_A_WEEK -> volunteersAllByHabitsInfo = volunteersAllByHabitsInfo.stream()
+                    .filter(v -> v.getAlcohol().equals(Alcohol.ONE_OR_TWO_TIMES_A_WEEK)).collect(Collectors.toList());
+        }
+        modelAndView.addObject("volunteersAllByHabitsInfo", volunteersAllByHabitsInfo);
+        modelAndView.setViewName("volunteersAllByHabitsInfo");
+        return modelAndView;
+    }
 
     @ModelAttribute
     public void addAttributes(Model model) {
         model.addAttribute("volunteer", new Volunteer());
         model.addAttribute("volunteerHabitsInfo", new VolunteerHabitsInfo());
         model.addAttribute("volunteerPrimaryHealthInfo", new VolunteerPrimaryHealthInfo());
-        model.addAttribute("newVolunteerHabitsInfo", new VolunteerHabitsInfo());
     }
 }
